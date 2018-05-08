802178961093d973aec328e9bfc0a6fe05d3cb21
AlexsaniKY <cerafem@gmail.com>
2018-05-08 19:06:20 -0400

    added querying to mysql repo

--- CSC313Final/Contact.aspx
+++ CSC313Final/Contact.aspx
@@ -3,13 +3,24 @@
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
     <h2><%: Title %>.</h2>
     <h3>Your contact page.</h3>
-    <address>
-        One Microsoft Way<br />
-        Redmond, WA 98052-6399<br />
-        <abbr title="Phone">P:</abbr>
-        425.555.0100
-    </address>
+    <%--<form id="searchForm" runat="server">--%>
+        Search for Equipment<br />
+        Name: &nbsp;
+        <asp:TextBox ID="searchName" runat="server"></asp:TextBox>
+        <br />
+        Manufacturer: &nbsp;
+        <asp:TextBox ID="searchManu" runat="server"></asp:TextBox>
+        <br />
+        Equipment Type: &nbsp;
+        <asp:TextBox ID="searchEquip" runat="server"></asp:TextBox>
+        <br />
+        Battery Type: &nbsp;
+        <asp:TextBox ID="searchBatt" runat="server"></asp:TextBox>
+        <br />
+        <asp:Button ID="searchButton" runat="server" OnClick="searchButton_Click" Text="Search" />
+    <%--</form>--%>
 
+    <asp:GridView ID="GridView1" runat="server"></asp:GridView>
     <address>
         <strong>Support:</strong>   <a href="mailto:Support@example.com">Support@example.com</a><br />
         <strong>Marketing:</strong> <a href="mailto:Marketing@example.com">Marketing@example.com</a>
--- CSC313Final/Contact.aspx.cs
+++ CSC313Final/Contact.aspx.cs
@@ -4,14 +4,44 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+using System.Data;
 
 namespace CSC313Final
 {
     public partial class Contact : Page
     {
-        protected void Page_Load(object sender, EventArgs e)
-        {
+        protected void Page_Load(object sender, EventArgs e) {
 
+        }
+
+        protected void searchButton_Click(object sender, EventArgs e)
+        {
+            string connStr = "server=127.0.0.1;user id=root;persistsecurityinfo=True;database=csc313finaldb;SslMode=none;";
+            MySqlConnection conn = new MySqlConnection(connStr);
+
+            try
+            {
+                conn.Open();
+                string sql = "select * from Equipment where name like @name and manufacturer like @manu and equipment_type like @equip and battery_type like @batt order by equipment_type, manufacturer, name;";
+
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@name", "%" + searchName.Text + "%");
+                cmd.Parameters.AddWithValue("@manu", "%" + searchManu.Text + "%");
+                cmd.Parameters.AddWithValue("@equip", "%"+ searchEquip.Text + "%");
+                cmd.Parameters.AddWithValue("@batt", "%" + searchBatt.Text + "%");
+
+                MySqlDataAdapter adp = new MySqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+
+                adp.Fill(ds);
+                GridView1.DataSource = ds;
+                GridView1.DataBind();
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex.ToString());
+            }
         }
     }
 }
